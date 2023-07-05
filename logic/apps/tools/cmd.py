485536520799cc85b0719e4820a2612fbbadb0b1
brian <blobo@redhat.com>
2023-07-05 17:08:38 -0300

    Borro if de prueba

--- logic/apps/tools/cmd.py
+++ logic/apps/tools/cmd.py
@@ -7,8 +7,6 @@ def exec(cmd: str, echo: bool = True) -> str:
     if echo:
         logger.info(cmd)
 
-    if 'docker compose' in cmd:
-        return ''
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     p.wait()
     return p.stdout
